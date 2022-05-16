455d0c22178181a58532d86aee18ff494f156b10
Mariusmivw <mvan-wij@student.codam.nl>
2022-05-16 13:40:28 +0200

    add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (char *s = argv[i]; *s; s++)
+			std::cout << static_cast<char>(std::toupper(*s));
+	}
+	std::cout << std::endl;
+	return (0);
+}
